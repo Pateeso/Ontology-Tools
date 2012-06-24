800afd2586ea6bd1cce6bfdb4f94760eccd484f8
Patesco Software Solutions, Inc <Ontology@patesco.ca>
2012-06-24 13:25:22 -0700

    Model for RF1

--- snomed/browser/models.py
+++ snomed/browser/models.py
@@ -8,3 +8,23 @@ class Concept(models.Model):
 	SNOMEDID=Models.CharField(max_length=50)
 	IsPrimative=Models.IntegerField()
 
+class Description(models.Model):
+	ConceptId=Models.ForeignKey(Concept,db_column='ConceptID')
+	DescriptionID=Models.CharField(max_length=50)
+	DescriptionStatus=Models.IntegerField()
+	Term=Models.CharField(max_length=4096)
+	InitialCapitalStatus=Models.IntegerField()
+	DescriptionType=Models.IntegerField()
+	LanguageCode=Models.CharField(max_length=25)
+
+class Relationship(models.Model):
+	RelationshipId=Models.CharField(max_length=50)
+	ConceptID1=Models.ForeignKey(Concept,db_column='ConceptID1')
+	RelationshipType=Models.ForeignKey(Concept,db_column='RelationshipType')
+	ConceptID2=Models.ForeignKey(Concept,db_column='ConceptID2')
+	CharacteristicType=Models.IntegerField()
+	Refinability=Models.IntegerField()
+	RelationshipGroup=Models.IntegerField()
+	
+
+
