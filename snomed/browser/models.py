8a5d04a84e68830c296d6e2d3cdef2a7fe43852a
Patesco Software Solutions, Inc <Ontology@patesco.ca>
2012-06-24 14:58:00 -0700

    Model for RF1

--- snomed/browser/models.py
+++ snomed/browser/models.py
@@ -1,30 +1,30 @@
 from django.db import models
 
 class Concept(models.Model):
-	ConceptId=Models.CharField(max_length=50)
-	ConceptStatus=Models.IntegerField()
-	FullySpecifiedName=Models.CharField(max_length=4096)
-	CTV3ID=Models.CharField(max_length=50)
-	SNOMEDID=Models.CharField(max_length=50)
-	IsPrimative=Models.IntegerField()
+	ConceptId=models.CharField(max_length=50,unique=True)
+	ConceptStatus=models.IntegerField()
+	FullySpecifiedName=models.CharField(max_length=4096)
+	CTV3ID=models.CharField(max_length=50)
+	SNOMEDID=models.CharField(max_length=50)
+	IsPrimative=models.IntegerField()
 
 class Description(models.Model):
-	ConceptId=Models.ForeignKey(Concept,db_column='ConceptID',to_field='ConceptId')
-	DescriptionID=Models.CharField(max_length=50)
-	DescriptionStatus=Models.IntegerField()
-	Term=Models.CharField(max_length=4096)
-	InitialCapitalStatus=Models.IntegerField()
-	DescriptionType=Models.IntegerField()
-	LanguageCode=Models.CharField(max_length=25)
+	ConceptId=models.ForeignKey(Concept,db_column='ConceptID',to_field='ConceptId')
+	DescriptionID=models.CharField(max_length=50)
+	DescriptionStatus=models.IntegerField()
+	Term=models.CharField(max_length=4096)
+	InitialCapitalStatus=models.IntegerField()
+	DescriptionType=models.IntegerField()
+	LanguageCode=models.CharField(max_length=25)
 
 class Relationship(models.Model):
-	RelationshipId=Models.CharField(max_length=50)
-	ConceptID1=Models.ForeignKey(Concept,db_column='ConceptID1',to_field='ConceptId')
-	RelationshipType=Models.ForeignKey(Concept,db_column='RelationshipType',to_field='ConceptId')
-	ConceptID2=Models.ForeignKey(Concept,db_column='ConceptID2',to_field='ConceptId')
-	CharacteristicType=Models.IntegerField()
-	Refinability=Models.IntegerField()
-	RelationshipGroup=Models.IntegerField()
+	RelationshipId=models.CharField(max_length=50)
+	ConceptID1=models.ForeignKey(Concept,db_column='ConceptID1',to_field='ConceptId', related_name='FirstConcept')
+	RelationshipType=models.ForeignKey(Concept,db_column='RelationshipType',to_field='ConceptId', related_name='RelationshipConcept')
+	ConceptID2=models.ForeignKey(Concept,db_column='ConceptID2',to_field='ConceptId', related_name='SecondConcept')
+	CharacteristicType=models.IntegerField()
+	Refinability=models.IntegerField()
+	RelationshipGroup=models.IntegerField()
 	
 
 
